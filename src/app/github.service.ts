562c74e2aecd200d73b58c52219d1d916ba01691
Abdi <oxabdi75@gmail.com>
2019-12-06 12:30:00 +0300

    added functions and import

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient, HttpHeaders} from "@angular/common/http"
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  private username = "Abdifatah135";
+  private client_id = "9f92ff4f90a8f81d0f87";
+  private client_secret= '0d35edd3ed1d0e746691e0eb8c76503fbbfa2bdc';
+  
 
-  constructor() { }
+  constructor(private _http:HttpClient) { 
+
+  }
 }
