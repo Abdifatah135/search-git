5e52136685028451ea2064cd462c506c5b91b1f3
Abdi <oxabdi75@gmail.com>
2019-12-06 12:38:00 +0300

    added imports to component ts

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService} from "../github.service";
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css']
+  styleUrls: ['./github.component.css'],
+  providers: [GithubService]
 })
 export class GithubComponent implements OnInit {
 
-  constructor() { }
+  constructor(private githubservice:GithubService) { }
 
   ngOnInit() {
   }
